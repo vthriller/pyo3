c01195a60620dafee76ce388a423011d8cd9c923
Travis CI User <travis@example.org>
2019-12-14 13:41:52 +0000

    Upload documentation for master

--- master/doc/implementors/core/marker/trait.StructuralEq.js
+++ master/doc/implementors/core/marker/trait.StructuralEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["pyo3"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"pyo3/buffer/enum.ElementType.html\" title=\"enum pyo3::buffer::ElementType\">ElementType</a>",synthetic:false,types:["pyo3::buffer::ElementType"]},];
+implementors["pyo3"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"pyo3/buffer/enum.ElementType.html\" title=\"enum pyo3::buffer::ElementType\">ElementType</a>","synthetic":false,"types":["pyo3::buffer::ElementType"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
