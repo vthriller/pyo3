5e74faaf83beac8cb2debf73cb5ab2287e10e7bc
Travis CI User <travis@example.org>
2020-01-11 06:50:06 +0000

    Upload documentation for master

--- master/doc/implementors/core/ops/deref/trait.DerefMut.js
+++ master/doc/implementors/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["pyo3"] = [{"text":"impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"pyo3/type_object/trait.PyTypeInfo.html\" title=\"trait pyo3::type_object::PyTypeInfo\">PyTypeInfo</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"pyo3/struct.PyRefMut.html\" title=\"struct pyo3::PyRefMut\">PyRefMut</a>&lt;'a, T&gt;","synthetic":false,"types":["pyo3::instance::PyRefMut"]}];
+implementors["pyo3"] = [{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pyo3/pyclass/trait.PyClass.html\" title=\"trait pyo3::pyclass::PyClass\">PyClass</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"pyo3/pyclass/struct.PyClassShell.html\" title=\"struct pyo3::pyclass::PyClassShell\">PyClassShell</a>&lt;T&gt;","synthetic":false,"types":["pyo3::pyclass::PyClassShell"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
