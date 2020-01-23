be485a0fefd08bba9477fb09896332e470a90f05
Travis CI User <travis@example.org>
2020-01-23 02:59:14 +0000

    Upload documentation for master

--- master/doc/implementors/core/ops/deref/trait.Deref.js
+++ master/doc/implementors/core/ops/deref/trait.Deref.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["pyo3"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pyo3/ffi/struct.PyDateTimeAPI.html\" title=\"struct pyo3::ffi::PyDateTimeAPI\">PyDateTimeAPI</a>","synthetic":false,"types":["pyo3::ffi::datetime::PyDateTimeAPI"]},{"text":"impl&lt;T:&nbsp;<a class=\"trait\" href=\"pyo3/pyclass/trait.PyClass.html\" title=\"trait pyo3::pyclass::PyClass\">PyClass</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"pyo3/pyclass/struct.PyClassShell.html\" title=\"struct pyo3::pyclass::PyClassShell\">PyClassShell</a>&lt;T&gt;","synthetic":false,"types":["pyo3::pyclass::PyClassShell"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
