332b449ae7eb0fc77617f14e447811fbd66f7b42
Travis CI User <travis@example.org>
2020-06-28 09:23:20 +0000

    Upload documentation for v0.11.0

---
+++ v0.11.0/doc/pyo3/pyclass/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["PyClass","If `PyClass` is implemented for `T`, then we can use `T` in the Python world, via `PyCell`."],["PyClassAlloc","This trait enables custom `tp_new`/`tp_dealloc` implementations for `T: PyClass`."]]});
